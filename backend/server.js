1c3871f9be008e14ebe5770c73d8c0f00bcae4bb
Om-Shinde144 <157940551+Om-Shinde144@users.noreply.github.com>
2025-04-08 22:31:20 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -182,6 +182,6 @@ app.post('/bid/:id', authenticate, async (req, res) => {
 });
 
 // Start the server
-app.listen(5001, () => {
+app.listen(5001, '0.0.0.0', () => {
   console.log('Server is running on port 5001');
 });
